1cc47b9f81b9924c8121e1f56ce4eb5852ed2b02
Adit Prasad Lohani <lohaniadit4133@gmail.com>
2023-09-01 22:43:14 +0545

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,324 @@
+const typingText = document.querySelector(".typing-textko p"),
+inpField = document.querySelector(".wrapperko .input-fieldko"),
+tryAgainBtn = document.querySelector(".contentko buttonko"),
+timeTag = document.querySelector(".timeko span b"),
+mistakeTag = document.querySelector(".mistakeko span"),
+wpmTag = document.querySelector(".wpmko span"),
+cpmTag = document.querySelector(".cpmko span");
+
+let timer,
+maxTime = 60,
+timeLeft = maxTime,
+charIndex = mistakes = isTyping = 0;
+
+function loadParagraph() {
+    const ranIndex = Math.floor(Math.random() * paragraphs.length);
+    typingText.innerHTML = "";
+    paragraphs[ranIndex].split("").forEach(char => {
+        let span = `<span>${char}</span>`
+        typingText.innerHTML += span;
+    });
+    typingText.querySelectorAll("span")[0].classList.add("active");
+    document.addEventListener("keydown", () => inpField.focus());
+    typingText.addEventListener("click", () => inpField.focus());
+}
+
+function initTyping() {
+    let characters = typingText.querySelectorAll("span");
+    let typedChar = inpField.value.split("")[charIndex];
+    if(charIndex < characters.length - 1 && timeLeft > 0) {
+        if(!isTyping) {
+            timer = setInterval(initTimer, 1000);
+            isTyping = true;
+        }
+        if(typedChar == null) {
+            if(charIndex > 0) {
+                charIndex--;
+                if(characters[charIndex].classList.contains("incorrect")) {
+                    mistakes--;
+                }
+                characters[charIndex].classList.remove("correct", "incorrect");
+            }
+        } else {
+            if(characters[charIndex].innerText == typedChar) {
+                characters[charIndex].classList.add("correct");
+            } else {
+                mistakes++;
+                characters[charIndex].classList.add("incorrect");
+            }
+            charIndex++;
+        }
+        characters.forEach(span => span.classList.remove("active"));
+        characters[charIndex].classList.add("active");
+
+        let wpm = Math.round(((charIndex - mistakes)  / 5) / (maxTime - timeLeft) * 60);
+        wpm = wpm < 0 || !wpm || wpm === Infinity ? 0 : wpm;
+        
+        wpmTag.innerText = wpm;
+        mistakeTag.innerText = mistakes;
+        cpmTag.innerText = charIndex - mistakes;
+    } else {
+        clearInterval(timer);
+        inpField.value = "";
+    }   
+}
+
+function initTimer() {
+    if(timeLeft > 0) {
+        timeLeft--;
+        timeTag.innerText = timeLeft;
+        let wpm = Math.round(((charIndex - mistakes)  / 5) / (maxTime - timeLeft) * 60);
+        wpmTag.innerText = wpm;
+    } else {
+        clearInterval(timer);
+    }
+}
+
+function resetGame() {
+    loadParagraph();
+    clearInterval(timer);
+    timeLeft = maxTime;
+    charIndex = mistakes = isTyping = 0;
+    inpField.value = "";
+    timeTag.innerText = timeLeft;
+    wpmTag.innerText = 0;
+    mistakeTag.innerText = 0;
+    cpmTag.innerText = 0;
+}
+
+loadParagraph();
+inpField.addEventListener("input", initTyping);
+tryAgainBtn.addEventListener("click", resetGame);
+//jnslsj
+//selecting all required elements
+const filterItem = document.querySelector(".items");
+const filterImg = document.querySelectorAll(".gallerykokira .imagekokira");
+
+window.onload = ()=>{ //after window loaded
+  filterItem.onclick = (selectedItem)=>{ //if user click on filterItem div
+    if(selectedItem.target.classList.contains("item")){ //if user selected item has .item class
+      filterItem.querySelector(".active").classList.remove("active"); //remove the active class which is in first item
+      selectedItem.target.classList.add("active"); //add that active class on user selected item
+      let filterName = selectedItem.target.getAttribute("data-name"); //getting data-name value of user selected item and store in a filtername variable
+      filterImg.forEach((image) => {
+        let filterImges = image.getAttribute("data-name"); //getting image data-name value
+        //if user selected item data-name value is equal to images data-name value
+        //or user selected item data-name value is equal to "all"
+        if((filterImges == filterName) || (filterName == "all")){
+          image.classList.remove("hide"); //first remove the hide class from the image
+          image.classList.add("show"); //add show class in image
+        }else{
+          image.classList.add("hide"); //add hide class in image
+          image.classList.remove("show"); //remove show class from the image
+        }
+      });
+    }
+  }
+  for (let i = 0; i < filterImg.length; i++) {
+    filterImg[i].setAttribute("onclick", "preview(this)"); //adding onclick attribute in all available images
+  }
+}
+
+//fullscreen image preview function
+//selecting all required elements
+const previewBox = document.querySelector(".preview-boxkokira"),
+categoryName = previewBox.querySelector(".titlekokira p"),
+previewImg = previewBox.querySelector("img"),
+closeIcon = previewBox.querySelector(".icon"),
+shadow = document.querySelector(".shadow");
+
+function preview(element){
+  //once user click on any image then remove the scroll bar of the body, so user can't scroll up or down
+  document.querySelector("body").style.overflow = "hidden";
+  let selectedPrevImg = element.querySelector("img").src; //getting user clicked image source link and stored in a variable
+  let selectedImgCategory = element.getAttribute("data-name"); //getting user clicked image data-name value
+  previewImg.src = selectedPrevImg; //passing the user clicked image source in preview image source
+  categoryName.textContent = selectedImgCategory; //passing user clicked data-name value in category name
+  previewBox.classList.add("show"); //show the preview image box
+  shadow.classList.add("show"); //show the light grey background
+  closeIcon.onclick = ()=>{ //if user click on close icon of preview box
+    previewBox.classList.remove("show"); //hide the preview box
+    shadow.classList.remove("show"); //hide the light grey background
+    document.querySelector("body").style.overflow = "auto"; //show the scroll bar on body
+  }
+}
+
+//jkljklsjlk
+const musicContainer = document.getElementById('music-container');
+const playBtn = document.getElementById('play');
+const prevBtn = document.getElementById('prev');
+const nextBtn = document.getElementById('next');
+
+const audio = document.getElementById('audio');
+const progress = document.getElementById('progress');
+const progressContainer = document.getElementById('progress-container');
+const title = document.getElementById('title');
+const cover = document.getElementById('cover');
+const currTime = document.querySelector('#currTime');
+const durTime = document.querySelector('#durTime');
+
+// Song titles
+const songs = ['hey', 'summer', 'ukulele'];
+
+// Keep track of song
+let songIndex = 2;
+
+// Initially load song details into DOM
+loadSong(songs[songIndex]);
+
+// Update song details
+function loadSong(song) {
+  title.innerText = song;
+  audio.src = `music/${song}.mp3`;
+  cover.src = `images/${song}.jpg`;
+}
+
+// Play song
+function playSong() {
+  musicContainer.classList.add('play');
+  playBtn.querySelector('i.fa').classList.remove('fa-play');
+  playBtn.querySelector('i.fa').classList.add('fa-pause');
+
+  audio.play();
+}
+
+// Pause song
+function pauseSong() {
+  musicContainer.classList.remove('play');
+  playBtn.querySelector('i.fa').classList.add('fa-play');
+  playBtn.querySelector('i.fa').classList.remove('fa-pause');
+
+  audio.pause();
+}
+
+// Previous song
+function prevSong() {
+  songIndex--;
+
+  if (songIndex < 0) {
+    songIndex = songs.length - 1;
+  }
+
+  loadSong(songs[songIndex]);
+
+  playSong();
+}
+
+// Next song
+function nextSong() {
+  songIndex++;
+
+  if (songIndex > songs.length - 1) {
+    songIndex = 0;
+  }
+
+  loadSong(songs[songIndex]);
+
+  playSong();
+}
+
+// Update progress bar
+function updateProgress(e) {
+  const { duration, currentTime } = e.srcElement;
+  const progressPercent = (currentTime / duration) * 100;
+  progress.style.width = `${progressPercent}%`;
+}
+
+// Set progress bar
+function setProgress(e) {
+  const width = this.clientWidth;
+  const clickX = e.offsetX;
+  const duration = audio.duration;
+
+  audio.currentTime = (clickX / width) * duration;
+}
+
+//get duration & currentTime for Time of song
+function DurTime (e) {
+	const {duration,currentTime} = e.srcElement;
+	var sec;
+	var sec_d;
+
+	// define minutes currentTime
+	let min = (currentTime==null)? 0:
+	 Math.floor(currentTime/60);
+	 min = min <10 ? '0'+min:min;
+
+	// define seconds currentTime
+	function get_sec (x) {
+		if(Math.floor(x) >= 60){
+			
+			for (var i = 1; i<=60; i++){
+				if(Math.floor(x)>=(60*i) && Math.floor(x)<(60*(i+1))) {
+					sec = Math.floor(x) - (60*i);
+					sec = sec <10 ? '0'+sec:sec;
+				}
+			}
+		}else{
+		 	sec = Math.floor(x);
+		 	sec = sec <10 ? '0'+sec:sec;
+		 }
+	} 
+
+	get_sec (currentTime,sec);
+
+	// change currentTime DOM
+	currTime.innerHTML = min +':'+ sec;
+
+	// define minutes duration
+	let min_d = (isNaN(duration) === true)? '0':
+		Math.floor(duration/60);
+	 min_d = min_d <10 ? '0'+min_d:min_d;
+
+
+	 function get_sec_d (x) {
+		if(Math.floor(x) >= 60){
+			
+			for (var i = 1; i<=60; i++){
+				if(Math.floor(x)>=(60*i) && Math.floor(x)<(60*(i+1))) {
+					sec_d = Math.floor(x) - (60*i);
+					sec_d = sec_d <10 ? '0'+sec_d:sec_d;
+				}
+			}
+		}else{
+		 	sec_d = (isNaN(duration) === true)? '0':
+		 	Math.floor(x);
+		 	sec_d = sec_d <10 ? '0'+sec_d:sec_d;
+		 }
+	} 
+
+	// define seconds duration
+	
+	get_sec_d (duration);
+
+	// change duration DOM
+	durTime.innerHTML = min_d +':'+ sec_d;
+		
+};
+
+// Event listeners
+playBtn.addEventListener('click', () => {
+  const isPlaying = musicContainer.classList.contains('play');
+
+  if (isPlaying) {
+    pauseSong();
+  } else {
+    playSong();
+  }
+});
+
+// Change song
+prevBtn.addEventListener('click', prevSong);
+nextBtn.addEventListener('click', nextSong);
+
+// Time/song update
+audio.addEventListener('timeupdate', updateProgress);
+
+// Click on progress bar
+progressContainer.addEventListener('click', setProgress);
+
+// Song ends
+audio.addEventListener('ended', nextSong);
+
+// Time of song
+audio.addEventListener('timeupdate',DurTime);
--- js/template.js
+++ js/template.js
@@ -20,7 +20,7 @@ function handleDeviceType() {
 
   // Call the handleDeviceType function on page load
 handleDeviceType();
-const backToTopButton  =document.querySelector("#back-to-top-btn");
+const backToTopButton = document.querySelector("#back-to-top-btn");
 window.addEventListener("scroll", scrollFunction);
 function scrollFunction(){
 	if (window.pageYOffset>300){
@@ -40,7 +40,249 @@ function isElementInViewport(element) {
         rect.right <= (window.innerWidth || document.documentElement.clientWidth)
     );
 }
+// tech skillsko
 
+
+let circularProgressAUTO = document.querySelector(".skills .contantootiner:nth-child(1) .circular-progress"),
+    progressValueAUTO = document.querySelector(".skills .contantootiner:nth-child(1) .progress-value");
+
+let circularProgressPython = document.querySelector(".skills .contantootiner:nth-child(2) .circular-progress"),
+    progressValuePython = document.querySelector(".skills .contantootiner:nth-child(2) .progress-value");
+
+let circularProgressPhotoshop = document.querySelector(".skills .contantootiner:nth-child(3) .circular-progress"),
+    progressValuePhotoshop = document.querySelector(".skills .contantootiner:nth-child(3) .progress-value");
+
+let circularProgressCircuit = document.querySelector(".skills .contantootiner:nth-child(4) .circular-progress"),
+    progressValueCircuit = document.querySelector(".skills .contantootiner:nth-child(4) .progress-value");
+
+let circularProgressWeb = document.querySelector(".skills .contantootiner:nth-child(5) .circular-progress"),
+    progressValueWeb = document.querySelector(".skills .contantootiner:nth-child(5) .progress-value");
+
+let circularProgressMath = document.querySelector(".skills .contantootiner:nth-child(6) .circular-progress"),
+    progressValueMath = document.querySelector(".skills .contantootiner:nth-child(6) .progress-value");
+
+let circularProgressEE = document.querySelector(".skills .contantootiner:nth-child(7) .circular-progress"),
+    progressValueEE = document.querySelector(".skills .contantootiner:nth-child(7) .progress-value");
+
+let progressStartValueAUTO = 0,
+    progressEndValueAUTO = 90,
+    speedHTML = 50;
+
+let progressStartValuePython = 0,
+    progressEndValuePython = 80,
+    speedPython = 50;
+
+let progressStartValuePhotoshop = 0,
+    progressEndValuePhotoshop = 70;
+
+let progressStartValueCircuit = 0,
+    progressEndValueCircuit = 75;
+
+let progressStartValueWeb = 0,
+    progressEndValueWeb = 85;
+
+let progressStartValueMath = 0,
+    progressEndValueMath = 95;
+
+let progressStartValueEE = 0,
+    progressEndValueEE = 75;
+
+let progressAUTO = setInterval(() => {
+    progressStartValueAUTO++;
+    
+    progressValueAUTO.textContent = `${progressStartValueAUTO}%`;
+    circularProgressAUTO.style.background = `conic-gradient(#a5a5a5 ${progressStartValueAUTO * 3.6}deg, #d3d3d3 0deg)`;
+    
+    if (progressStartValueAUTO === progressEndValueAUTO) {
+        clearInterval(progressAUTO);
+    }
+}, speedHTML);
+
+let progressWeb = setInterval(() => {
+    progressStartValueWeb++;
+
+    progressValueWeb.textContent = `${progressStartValueWeb}%`;
+    circularProgressWeb.style.background = `conic-gradient(#a5a5a5 ${progressStartValueWeb * 3.6}deg, #d3d3d3 0deg)`;
+
+    if (progressStartValueWeb === progressEndValueWeb) {
+        clearInterval(progressWeb);
+    }
+}, speedHTML);
+let progressMath = setInterval(() => {
+    progressStartValueMath++;
+
+    progressValueMath.textContent = `${progressStartValueMath}%`;
+    circularProgressMath.style.background = `conic-gradient(#a5a5a5 ${progressStartValueMath * 3.6}deg, #d3d3d3 0deg)`;
+
+    if (progressStartValueMath === progressEndValueMath) {
+        clearInterval(progressMath);
+    }
+}, speedHTML);
+let progressEE = setInterval(() => {
+    progressStartValueEE++;
+
+    progressValueEE.textContent = `${progressStartValueEE}%`;
+    circularProgressEE.style.background = `conic-gradient(#a5a5a5 ${progressStartValueEE * 3.6}deg, #d3d3d3 0deg)`;
+
+    if (progressStartValueEE === progressEndValueEE) {
+        clearInterval(progressEE);
+    }
+}, speedHTML);
+
+let progressCircuit = setInterval(() => {
+    progressStartValueCircuit++;
+
+    progressValueCircuit.textContent = `${progressStartValueCircuit}%`;
+    circularProgressCircuit.style.background = `conic-gradient(#a5a5a5 ${progressStartValueCircuit * 3.6}deg, #d3d3d3 0deg)`;
+
+    if (progressStartValueCircuit === progressEndValueCircuit) {
+        clearInterval(progressCircuit);
+    }
+}, speedHTML);
+let progressPhotoshop = setInterval(() => {
+    progressStartValuePhotoshop++;
+
+    progressValuePhotoshop.textContent = `${progressStartValuePhotoshop}%`;
+    circularProgressPhotoshop.style.background = `conic-gradient(#a5a5a5 ${progressStartValuePhotoshop * 3.6}deg, #d3d3d3 0deg)`;
+
+    if (progressStartValuePhotoshop === progressEndValuePhotoshop) {
+        clearInterval(progressPhotoshop);
+    }
+}, speedHTML);
+
+let progressPython = setInterval(() => {
+    progressStartValuePython++;
+
+    progressValuePython.textContent = `${progressStartValuePython}%`;
+    circularProgressPython.style.background = `conic-gradient(#a5a5a5 ${progressStartValuePython * 3.6}deg, #d3d3d3 0deg)`; // Adjusted value for Python
+
+    if (progressStartValuePython === progressEndValuePython) {
+        clearInterval(progressPython);
+    }
+}, speedPython);
+
+// skillsko
+//interskill
+let circularProgressLeadership = document.querySelector(".skills1 .contantootiner1:nth-child(1) .circular-progress1"),
+    progressValueLeadership = document.querySelector(".skills1 .contantootiner1:nth-child(1) .progress-value1");
+
+let progressStartValueLeadership = 0,
+    progressEndValueLeadership = 90;
+
+let progressLeadership = setInterval(() => {
+    progressStartValueLeadership++;
+    
+    progressValueLeadership.textContent = `${progressStartValueLeadership}%`;
+    circularProgressLeadership.style.background = `conic-gradient(#a5a5a5 ${progressStartValueLeadership * 3.6}deg, #d3d3d3 0deg)`;
+    
+    if (progressStartValueLeadership === progressEndValueLeadership) {
+         clearInterval(progressLeadership);
+    }
+}, speedHTML);
+let circularProgressMHC = document.querySelector(".skills1 .contantootiner1:nth-child(2) .circular-progress1"),
+    progressValueMHC = document.querySelector(".skills1 .contantootiner1:nth-child(2) .progress-value1");
+
+let progressStartValueMHC = 0,
+    progressEndValueMHC = 95;
+
+let progressMHC = setInterval(() => {
+    progressStartValueMHC++;
+    
+    progressValueMHC.textContent = `${progressStartValueMHC}%`;
+    circularProgressMHC.style.background = `conic-gradient(#a5a5a5 ${progressStartValueMHC * 3.6}deg, #d3d3d3 0deg)`;
+    
+    if (progressStartValueMHC === progressEndValueMHC) {
+         clearInterval(progressMHC);
+    }
+}, speedHTML);
+let circularProgressFL = document.querySelector(".skills1 .contantootiner1:nth-child(3) .circular-progress1"),
+    progressValueFL = document.querySelector(".skills1 .contantootiner1:nth-child(3) .progress-value1");
+
+let progressStartValueFL = 0,
+    progressEndValueFL = 70;
+
+let progressFL = setInterval(() => {
+    progressStartValueFL++;
+    
+    progressValueFL.textContent = `${progressStartValueFL}%`;
+    circularProgressFL.style.background = `conic-gradient(#a5a5a5 ${progressStartValueFL * 3.6}deg, #d3d3d3 0deg)`;
+    
+    if (progressStartValueFL === progressEndValueFL) {
+         clearInterval(progressFL);
+    }
+}, speedHTML);
+let circularProgressMUN = document.querySelector(".skills1 .contantootiner1:nth-child(4) .circular-progress1"),
+    progressValueMUN = document.querySelector(".skills1 .contantootiner1:nth-child(4) .progress-value1");
+
+let progressStartValueMUN = 0,
+    progressEndValueMUN = 85;
+
+let progressMUN = setInterval(() => {
+    progressStartValueMUN++;
+    
+    progressValueMUN.textContent = `${progressStartValueMUN}%`;
+    circularProgressMUN.style.background = `conic-gradient(#a5a5a5 ${progressStartValueMUN * 3.6}deg, #d3d3d3 0deg)`;
+    
+    if (progressStartValueMUN === progressEndValueMUN) {
+         clearInterval(progressMUN);
+    }
+}, speedHTML);
+let circularProgressTL = document.querySelector(".skills1 .contantootiner1:nth-child(5) .circular-progress1"),
+    progressValueTL = document.querySelector(".skills1 .contantootiner1:nth-child(5) .progress-value1");
+
+let progressStartValueTL = 0,
+    progressEndValueTL = 90;
+
+let progressTL = setInterval(() => {
+    progressStartValueTL++;
+    
+    progressValueTL.textContent = `${progressStartValueTL}%`;
+    circularProgressTL.style.background = `conic-gradient(#a5a5a5 ${progressStartValueTL * 3.6}deg, #d3d3d3 0deg)`;
+    
+    if (progressStartValueTL === progressEndValueTL) {
+         clearInterval(progressTL);
+    }
+}, speedHTML);
+let circularProgressPM = document.querySelector(".skills1 .contantootiner1:nth-child(6) .circular-progress1"),
+    progressValuePM = document.querySelector(".skills1 .contantootiner1:nth-child(6) .progress-value1");
+
+let progressStartValuePM = 0,
+    progressEndValuePM = 70;
+
+let progressPM = setInterval(() => {
+    progressStartValuePM++;
+    
+    progressValuePM.textContent = `${progressStartValuePM}%`;
+    circularProgressPM.style.background = `conic-gradient(#a5a5a5 ${progressStartValuePM * 3.6}deg, #d3d3d3 0deg)`;
+    
+    if (progressStartValuePM === progressEndValuePM) {
+         clearInterval(progressPM);
+    }
+}, speedHTML);
+//interskill
+//porjec
+const allImages = document.querySelectorAll(".images .img");
+const lightbox = document.querySelector(".lightbox");
+const closeImgBtn = lightbox.querySelector(".close-icon");
+
+allImages.forEach(img => {
+    // Calling showLightBox function with passing clicked img src as argument
+    img.addEventListener("click", () => showLightbox(img.querySelector("img").src));
+});
+
+const showLightbox = (img) => {
+    // Showing lightbox and updating img source
+    lightbox.querySelector("img").src = img;
+    lightbox.classList.add("show");
+    document.body.style.overflow = "hidden";
+}
+
+closeImgBtn.addEventListener("click", () => {
+    // Hiding lightbox on close icon click
+    lightbox.classList.remove("show");
+    document.body.style.overflow = "auto";
+});
+//porjec
 // Function to handle scroll event
 function handleScroll() {
     var lkow = document.querySelector('.lkow');
@@ -175,3 +417,6 @@ jQuery(document).ready(function($) {
 		 initio_parallax_animation();
 	});
 });
+//ioljlk
+
+//jkjl
